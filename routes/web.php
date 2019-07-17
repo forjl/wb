a115f32bcfbd5bcceb3dee03cec3325cdb99bcab
forjl <748409412@qq.com>
2019-07-18 02:03:58 +0800

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,13 @@ Route::get('/help','StaticPagesController@help')->name('help');
 Route::get('/about','StaticPagesController@about')->name('about');
 
 Route::get('/signup','UsersController@create')->name('signup');
+
+Route::resource('users', 'UsersController');
+//resource等于下面7行代码
+// Route::get('/users','UsersController@index')->name('users.index');
+// Route::get('users/{user}','UsersController@show')->name('users.show');
+// Route::get('users/create','UsersController@create')->name('users.create');
+// Route::post('users/','UsersController@store')->name('users.store');
+// Route::get('users/{user}/edit,UserController@edit')->name('users.edit');
+// Route::put('users/{user}','UsersController@update')->name('users.update');
+// Route::delete('users/{user}','UsersController@destroy')->name('users.destroy');
