d7d1b51169cc16a3da48883889ec6a6af7908389
forjl <748409412@qq.com>
2019-07-18 14:28:44 +0800

    增加CSRF验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,4 +16,14 @@ class UsersController extends Controller
     {
         return view('users.show',compact('user'));
     }
+
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'email' => 'email|required|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
+    }
 }
